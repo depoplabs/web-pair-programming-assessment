b4cd39756f15b86f4cfd405b3139841befb48320
depop-james-hughes <james.hughes@depop.com>
2022-02-23 10:29:18 +0000

    Remove rendering the product card

--- depop-app/src/components/ProductsContainer.js
+++ depop-app/src/components/ProductsContainer.js
@@ -7,6 +7,7 @@ import "./ProductsContainer.css"
 export const ProductsContainer = () => {
 
     const {products} = useGetProducts('https://5c78274f6810ec00148d0ff1.mockapi.io/api/v1/products');
+    console.log(products)
 
     return (
         <React.Fragment>
@@ -14,9 +15,6 @@ export const ProductsContainer = () => {
                 <div className="subTitle"><b>Results</b></div>
             }
             <div className="productsContainer">
-                {products && products.map(p => (
-                    <ProductCard product={p} key={p.id}/>
-                ))}
             </div>
         </React.Fragment>
     )
